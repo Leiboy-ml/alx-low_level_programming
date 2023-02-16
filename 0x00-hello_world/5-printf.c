b6ae917c0f7c06d27e5d48e2cd9a13b9b49fe2b1
Martin Leitich <mleitich@outlook.com>
2023-02-16 10:00:53 -0800

    corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void) /* main funtion*/
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+* Return: Always 0 (Success)
+ */
+int main(void)
 
 printf("with proper grammar, but the outcome is a piece of art\n,");
 
