a063bcc284094c828a31c861dd76260047310892
Martin Leitich <mleitich@outlook.com>
2023-02-22 05:02:36 -0800

    correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,22 @@ int print_sign(int n);
 {
 	if (n > 0)
         {
-	_putchar("%d is greater than zero")
+	_putchar("%d is greater than zero");
 	
 	return (1);
+
         }
         else if (n < 0)
+
+		_putchar("%d is less than zero\n");
+		return (-1);
         {
 
-                return (-1);
+                
         }
         else
         {
-                return (0);
+		_putchar("%d is zero\n");
+		return (0);
         }
 }
