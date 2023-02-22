fb9d94be723dd2ef263648240a1d43d7dc7ff1d5
Martin Leitich <mleitich@outlook.com>
2023-02-22 09:54:29 -0800

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer one
+ * @x: integer two
+ * Return: sum
+ */
+
+int add(int a, int x)
+{
+	int sum = 0;
+
+	sum = a + x;
+	return (sum);
+}
+
