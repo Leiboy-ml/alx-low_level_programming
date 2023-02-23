1dc41bc174a3b59374025cb271a4b726973ffdde
Martin Leitich <mleitich@outlook.com>
2023-02-23 05:25:58 -0800

    task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	for (c = '0';c <= '9'; c++)
+	{
+		return (1);
+	}
+	return (0);
