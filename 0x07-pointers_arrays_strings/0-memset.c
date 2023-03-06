a533474a4696cfa88c3a5149c100445fb5f48d73
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-06 12:08:04 +0300

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - Function  to fill memory with a constant byte
+ * @s: Starting address
+ * @b: The byte to fill with
+ * @n: Number of bytes
+ * Return: s (start address)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0 ;
+
+	for (i = 0; i < n; i++);
+	{
+		*s = b;
+	}
+
+	return (s);
+}
