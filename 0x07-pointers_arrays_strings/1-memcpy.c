f67c9e6a35ef2250ea6a08786613f5a5fc9d1253
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-06 13:40:57 +0300

    task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_memcpy - copies memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src
+ *
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x;
+
+	x = 0;
+
+	for (x =0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+
+	return (dest);
+}
