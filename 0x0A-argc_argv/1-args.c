7fd00ca6a91876800000ab073290906a8a0db03a
Martin Leitich <mleitich@outlook.com>
2023-03-11 06:48:56 -0800

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,14 +9,18 @@
  * return: 0
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++);
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
 	{
-		printf("%d\n", i);
+		for (i = 0; *argv; i++, argv++)
+			;
 
+		printf("%d\n", i - 1);
 	}
 
 	return (0);
