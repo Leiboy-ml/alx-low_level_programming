e46bec89f94a5b2eaba15b2482ed6b84a89a1fe2
Martin Leitich <mleitich@outlook.com>
2023-03-11 05:26:32 -0800

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: holds a string of arrays, index o contains the program name.
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	printf("%s\n",argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+README file for arg-c/arg_v task
