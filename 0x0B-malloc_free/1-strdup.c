221123f3ae06856dba2b2cfff1af5f38ea3bda22
Martin Leitich <mleitich@outlook.com>
2023-03-14 08:43:34 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string in a new location
+ * @str: the string in question
+ * Return: NULL or pointer
+ */
+
+char *_strdup(char *str)
+{
+	char n;
+	int str_len;
+	int x;
+	char *new;
+
+	str = malloc((sizeof(n) * str_len));
+
+	if (str == NULL)
+	{
+		return ('\0');
+	}
+
+	for (x = 0; x <= str_len; x++)
+
+	str[x] = *new;
+}
