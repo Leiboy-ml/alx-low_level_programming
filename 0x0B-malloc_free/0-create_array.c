852a1ce9fabe70aecfc88896b8df31534bf6cd29
Martin Leitich <mleitich@outlook.com>
2023-03-14 06:36:40 -0700

    task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,15 +19,16 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc((sizeof(c) * size));
 
-	array[i] = c;
 
 	if (size == 0)
 		return ('\0');
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
 		return (array);
 	}
+
+	array[i] = c;
 
 	return ('\0');
 }
