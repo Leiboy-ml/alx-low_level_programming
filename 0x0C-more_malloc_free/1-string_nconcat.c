a73726866e8a239a59d30228d0dac926bc2a14b5
Martin Leitich <mleitich@outlook.com>
2023-03-16 11:10:11 -0700

    task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: size of s2 to concatenate
+ * Return: pointer or NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	unsigned int i;
+
+	s1 = malloc(sizeof(s1));
+
+	s2 = malloc(sizeof(s2));
+
+	for (i = 0; i < n; i++)
+	{
+		if (n < strlen(s2))
+		{
+			strcat(s1, (s2 + n));
+		}
+
+		if (n >= strlen(s2))
+		{
+			strcat(s1, s2);
+		}
+
+			return (s1);
+
+		if (s1 || s2 == NULL)
+		{
+			free(s1);
+			free(s2);
+		}
+	}
+
+		return ('\0');
+
+}
