d9992d1c678d773416c6df1deae799841b053568
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-20 23:05:12 +0300

    message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
 
+/**
+ * new_dog - creates new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's ownwer
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
