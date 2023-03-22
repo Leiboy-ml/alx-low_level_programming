94f0c79032bead997447180ef758ffcd475ad607
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-22 23:20:58 +0300

    task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int j;
 
-	if (array == NULL || cmp == NULL
-			)
+	if (array == NULL || cmp == NULL)
+
 		return (-1);
 
 	for (j = 0; j < size; j++)
 	{
-		if ((cmp(array[j]) != 0)
+		if (cmp(array[j]) != 0)
 				return (j);
 	}
 
