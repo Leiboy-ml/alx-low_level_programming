9d1c3158c3568b1fe73f99266dffb1630f7ee9d9
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-30 21:23:15 +0300

    task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints a string before executing main
+ * Return: nothing
+ */
+
+Void print_before()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
