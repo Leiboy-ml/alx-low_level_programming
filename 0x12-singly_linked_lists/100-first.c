e150738bfcbef32e4692f2c748fe19fd5e846a79
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-03-30 21:33:15 +0300

    task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) before_main (void);
+void __attribute__((constructor)) before_main(void);
 
 /**
- * before_main - prints a string before executing main
- * Return: nothing
+ * before_main - Prints a string before the
+ *        main function is executed.
  */
-
-void print_before(void)
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n";
-		"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
