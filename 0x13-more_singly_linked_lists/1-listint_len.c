6c5b14783ae799f9a26d6a7b4c9c6f3c09b9407d
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-03 21:33:49 +0300

    task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - Prints all the elements  if a listint_t list.
+ * @h: A pointer to the had of the list_t list.
+ *
+ * Return: The number elements in the list_t list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int j = 0;
+
+	for (h != NULL; j++;)
+	{
+		h = h->next;
+	}
+
+	return (j);
+}
