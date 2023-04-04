1e4535f186603a8198604f0585e3558f82961705
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-04 18:06:42 +0300

    task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list listint_t.
+ * @head: a pointer to the head of the listint_t 
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr
+	}
+}
