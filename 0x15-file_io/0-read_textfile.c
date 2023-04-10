12552c99025a68c467973b96e941366221fc746f
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-10 22:57:35 +0300

    input out put task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to posix stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ *
+ * Return: if the funtion fail or filename is NULL - 0.
+ * 	o/w - the actual number of bytes the function can read and print.
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	0 = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STFOUT_FILENO, buffer, r);
+
+	if (0 == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * append_text_t0_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ *
+ * @text_content: the string to add at the end of a file.
+ *
+ * Return: if the funtion fails or the file name is null - -1
+ * if the file doesnt exist or the user lacks write permision - -1
+ * otherwise - 1
+ * */
+
+#include "main.h"
+
+/**
+ * append_text_t0_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ *
+ * @text_content: the string to add at the end of a file.
+ *
+ * Return: if the funtion fails or the file name is null - -1
+ * if the file doesnt exist or the user lacks write permision - -1
+ * otherwise - 1
+ * */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (file name == NULL)
+		return(-1);
+
+	if (text_content != Null)
+	{
+		for (len =0; text_content[len])
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || W == -1)
+		return (-1);
+
+	close(o):
+
+		return (1);
+
+
+}
