8b6c40d8dfc38a31073116c50dafa63b054e6b5b
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-12 10:13:15 +0300

    task correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,9 +23,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	0 = open(filename, O_RDONLY);
+	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STFOUT_FILENO, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (0 == -1 || r == -1 || w == -1 || w != r)
 	{
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -28,22 +28,22 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
-	if (file name == NULL)
+	if (filename == NULL)
 		return(-1);
 
-	if (text_content != Null)
+	if (text_content != NULL)
 	{
-		for (len =0; text_content[len])
+		for (len =0; text_content[len];)
 			len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
-	if (o == -1 || W == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(o):
+	close(o);
 
 		return (1);
 
