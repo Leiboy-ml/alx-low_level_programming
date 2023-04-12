7c607526002dc5cee4db966ad05a69dfd3832b5a
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-12 12:15:34 +0300

    correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,15 +16,15 @@ char *create_buffer(char *file)
 {
 	char *buffer;
 	
-	buffer = malloc(sizeof(char)*1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-		if (buffer == NULL)
-		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", file);
-			exit(99);
-		}
-		return (buffer);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
 }
 
 /**
@@ -35,11 +35,12 @@ char *create_buffer(char *file)
 void close_file(int fd)
 {
 	int c;
+
 	c = close(fd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Erroe: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -61,20 +62,19 @@ int main(int argc, char *argv[])
 	if(argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
-}
 
 	buffer = create_buffer(argv[2]);
-	from = open(arg[1], O_RDONLY);
+	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == 1 || r ==-1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from the file %s\n", arg[1]);
+				"Error: Can't read from the file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", arg[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	}while (r > 0);
+	} while (r > 0);
 
 	free(buffer);
 	close_file(from);
