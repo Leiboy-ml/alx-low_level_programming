9d6d3ba78ba9014daf9c288d761612968bd3dbbc
Leiboy-ml <122888165+Leiboy-ml@users.noreply.github.com>
2023-04-12 11:45:26 +0300

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-		{
-			while (text_content[len])
-				len++;
-		}
+	{
+		while (text_content[len])
+			len++;
+	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
